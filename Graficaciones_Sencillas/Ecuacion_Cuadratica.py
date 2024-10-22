41a343c7349acacbfc2d76343b0891388b9973e6
Jonathan_Hernandez_Mayen <91714580+Jona163@users.noreply.github.com>
2024-10-22 07:17:15 -0600

    Update Ecuacion_Cuadratica.py

--- Graficaciones_Sencillas/Ecuacion_Cuadratica.py
+++ Graficaciones_Sencillas/Ecuacion_Cuadratica.py
@@ -6,3 +6,13 @@ from matplotlib import pyplot
 a=float(input("ingrese el valor de (a):\n"))
 b=float(input("ingrese el valor de (b):\n"))
 c=float(input("ingrese el valor de (c):\n"))
+
+disc=(b**2)-(4*a*c)
+raiz=(disc)**(0.5)
+
+
+if(disc>0):
+    x1=((-b)+raiz)/(2*a)
+    x2=((-b)-raiz)/(2*a)
+    print("x1= ",x1)
+    print("x2= ",x2)
