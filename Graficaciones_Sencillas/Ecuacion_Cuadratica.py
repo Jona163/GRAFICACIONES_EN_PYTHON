0aceac3b7e3343d6cc10ae4c72fed443f5bcbc3f
Jonathan_Hernandez_Mayen <91714580+Jona163@users.noreply.github.com>
2024-10-22 07:16:59 -0600

    Update Ecuacion_Cuadratica.py

--- Graficaciones_Sencillas/Ecuacion_Cuadratica.py
+++ Graficaciones_Sencillas/Ecuacion_Cuadratica.py
@@ -1,1 +1,8 @@
+#Ecuacion Cuadratica
+import math as m
+import numpy as np
+from matplotlib import pyplot
 
+a=float(input("ingrese el valor de (a):\n"))
+b=float(input("ingrese el valor de (b):\n"))
+c=float(input("ingrese el valor de (c):\n"))
