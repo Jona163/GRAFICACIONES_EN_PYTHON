6042b53a9ad71fc10c2f8e42b9f423560c81ec03
Jonathan_Hernandez_Mayen <91714580+Jona163@users.noreply.github.com>
2024-10-22 07:23:30 -0600

    Update Graf_Polinomial_Anim.py

--- Graficaciones_Sencillas/Graf_Polinomial_Anim.py
+++ Graficaciones_Sencillas/Graf_Polinomial_Anim.py
@@ -10,3 +10,10 @@ t = np.linspace(3,0,0)
 
 X, T = np.meshgrid(x,t)
 Y = np.sin(2*np.pi(X+T))
+
+#Creamos objeto "timeline"
+timeline = amp.Timeline(t, units="s",fps=60)
+
+#Generamos animacion
+block = amp.blocks.Line(X,Y,marker=".",linestyle="-",color="r")
+anim = amp.Animation([block],timeline)
