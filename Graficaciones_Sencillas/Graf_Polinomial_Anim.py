d23cc48e3881a57fb460bb77f7163fc53bbbaa7d
Jonathan_Hernandez_Mayen <91714580+Jona163@users.noreply.github.com>
2024-10-22 07:23:20 -0600

    Update Graf_Polinomial_Anim.py

--- Graficaciones_Sencillas/Graf_Polinomial_Anim.py
+++ Graficaciones_Sencillas/Graf_Polinomial_Anim.py
@@ -3,3 +3,10 @@ from turtle import color
 import numpy as np
 import matplotlib as ptl
 import animatplot as amp
+
+#Introducimos datos.
+x = np.linspace(1,1,3)
+t = np.linspace(3,0,0)
+
+X, T = np.meshgrid(x,t)
+Y = np.sin(2*np.pi(X+T))
