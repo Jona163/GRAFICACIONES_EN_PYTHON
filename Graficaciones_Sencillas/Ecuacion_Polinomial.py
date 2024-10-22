7948881ee7cf5b8169b6a7c342cdfb2706b43940
Jonathan_Hernandez_Mayen <91714580+Jona163@users.noreply.github.com>
2024-10-22 07:19:16 -0600

    Update Ecuacion_Polinomial.py

--- Graficaciones_Sencillas/Ecuacion_Polinomial.py
+++ Graficaciones_Sencillas/Ecuacion_Polinomial.py
@@ -1,1 +1,17 @@
 
+#POLINOMIAL XD
+
+import numpy as np
+import animatplot as amp
+from sklearn import datasets, linear_model
+import matplotlib.pyplot as plt
+########## PREPARAR LA DATA ##########
+#Importamos los datos de la misma librería de scikit-learn
+boston = datasets.load_boston()
+print(boston)
+print()
+########## ENTENDIMIENTO DE LA DATA ##########
+#Verifico la información contenida en el dataset
+print('Información en el dataset:')
+print(boston.keys())
+print()
