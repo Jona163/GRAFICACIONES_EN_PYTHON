778b3309a8c32d2b9c3eb50c52b1c0b79f07b5e4
Jonathan_Hernandez_Mayen <91714580+Jona163@users.noreply.github.com>
2024-10-22 07:21:39 -0600

    Update Corazon_Parametrico.py

--- Graficaciones_Sencillas/Corazon_Parametrico.py
+++ Graficaciones_Sencillas/Corazon_Parametrico.py
@@ -8,3 +8,11 @@ def curveMove():
         turtle.right(1)
         turtle.forward(1)
 
+
+def drawHeart():
+    turtle.title("")
+    turtle.speed(1) # La velocidad del cepillo se ajusta al máximo
+    turtle.color('red','pink')
+    turtle.begin_fill()
+    turtle.left(140) # Gire 140 grados en sentido antihorario
+    turtle.forward(111.65) # Avanzar 111,65 píxeles
