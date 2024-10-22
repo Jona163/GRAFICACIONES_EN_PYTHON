c8872238f546b95cd02b026bfcfc89476f0077a7
Jonathan_Hernandez_Mayen <91714580+Jona163@users.noreply.github.com>
2024-10-22 07:21:48 -0600

    Update Corazon_Parametrico.py

--- Graficaciones_Sencillas/Corazon_Parametrico.py
+++ Graficaciones_Sencillas/Corazon_Parametrico.py
@@ -16,3 +16,13 @@ def drawHeart():
     turtle.begin_fill()
     turtle.left(140) # Gire 140 grados en sentido antihorario
     turtle.forward(111.65) # Avanzar 111,65 píxeles
+    curveMove() # Dibuja una curva
+    turtle.left(120) # Gire 120 grados en sentido antihorario
+    curveMove() # Continuar dibujando la curva
+    turtle.forward(111.65) # Avanzar 111,65 píxeles
+    turtle.end_fill()
+    time.sleep(20)
+    
+
+if __name__ == '__main__':
+    drawHeart()
