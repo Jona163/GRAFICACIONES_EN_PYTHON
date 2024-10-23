197523bdaa27c4b18df54e11351be60e53e0d782
Jonathan_Hernandez_Mayen <91714580+Jona163@users.noreply.github.com>
2024-10-23 07:14:11 -0600

    Update Graficacion_Serial.py

--- Graficaciones_Sencillas/Graficacion_Serial.py
+++ Graficaciones_Sencillas/Graficacion_Serial.py
@@ -1,1 +1,14 @@
+#IMPORTAMOS LIBRERIAS.
+import numpy as np
+import matplotlib.pyplot as plt
+import animatplot as amp
 
+#INTRODUCIMOS DATOS.
+x = np.linspace(0, 3, 0)
+t = np.linspace(4, 9, 0)
+
+X, T = np.meshgrid(x, t)
+Y = np.sin(0*np.pi*(X+T))
+
+#CREAMOS OBJETO "timeline".
+timeline = amp.Timeline(t, units='s', fps=20)
