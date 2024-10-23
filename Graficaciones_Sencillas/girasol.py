08ed8c5db1dc8f4e182874be8e530816dd3569ea
Jonathan_Hernandez_Mayen <91714580+Jona163@users.noreply.github.com>
2024-10-23 10:12:46 -0600

    Update girasol.py

--- Graficaciones_Sencillas/girasol.py
+++ Graficaciones_Sencillas/girasol.py
@@ -1,1 +1,9 @@
+import math
+import turtle
 
+# Configuración inicial de la ventana y la tortuga
+turtle.bgcolor("black")
+turtle.shape("turtle")
+turtle.speed(0)
+turtle.fillcolor("brown")
+turtle.color("brown")  # Color del centro del girasol
