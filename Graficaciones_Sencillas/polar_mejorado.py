fb9fef1df36bf17a368137ea0f5b54f7010f57a7
Jonathan_Hernandez_Mayen <91714580+Jona163@users.noreply.github.com>
2024-10-23 10:18:49 -0600

    Update polar_mejorado.py

--- Graficaciones_Sencillas/polar_mejorado.py
+++ Graficaciones_Sencillas/polar_mejorado.py
@@ -8,3 +8,9 @@ plt.axes(projection='polar')
 # Parámetros
 x = 5
 y = 8
+
+# Rango de valores para los ángulos (rad)
+rads = np.arange(0, 8 * np.pi, 0.01)  # Empezamos desde 0 para cubrir todo el rango
+
+# Calcular los valores de r para cada radian
+r = (x * y) / np.sqrt((x * np.sin(rads))**2 + (y * np.cos(rads))**2)
