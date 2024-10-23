142ac9715bcd0e3da12e3dc3fd041e734f1ceef7
Jonathan_Hernandez_Mayen <91714580+Jona163@users.noreply.github.com>
2024-10-23 07:15:30 -0600

    Update Grafica_polar.py

--- Graficaciones_Sencillas/Grafica_polar.py
+++ Graficaciones_Sencillas/Grafica_polar.py
@@ -1,1 +1,11 @@
+import numpy as np
+import matplotlib.pyplot as plt
 
+theta = np.linspace(0,4*np.pi)
+r = 32 + 4*theta
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection="polar")
+ax.plot(theta,r)
+
+plt.show()
